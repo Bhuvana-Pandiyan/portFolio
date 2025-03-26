0fbee5f6b529a48b2aa4740e7080c559ef20aabb
Meenakandasamy <meenakandasamy2002@gmail.com>
2025-03-26 19:15:22 +0530

    updated code

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -2,7 +2,7 @@ import Image from "../Image/girl.png"
 import { FaLinkedin } from "react-icons/fa";
 import { SiLeetcode } from "react-icons/si";
 export default function hero(){
-    return<section className='flex flex-col md:flex-row px-5 py-32 mobile-navd justify-center'>
+    return<section className='flex flex-col md:flex-row px-4 py-32 mobile-navd justify-center'>
     <div className='md:w-1/2 flex flex-col'>
          <h1 className=' text-white text-6xl font-hero-font'>Hi, <br/> Im <span className='text-white'>Meena</span> Knadasamy
              {/* <p className='text-2xl'>{config.subtitle}</p> */}
