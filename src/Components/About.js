e44173965c6df7677327a180d64dbeeb8b2eafd4
Meenakandasamy <meenakandasamy2002@gmail.com>
2025-04-02 07:07:39 +0530

    About screen updated code

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,14 +1,16 @@
 import Image from"../Image/girl1.png"
 export function About(){
-    return<section>
-        <div className="w-1/3">
-<img src={Image}/>
+    return<section className=" flex flex-col md:flex-row px-4 py-32 mobile-navd justify-center">
+        <div className="w-1/2" >
+<img src={Image} className="w-1/2"/>
         </div>
-        <div className="w-1/3">
-            <div className="flex  flex-col justify-center">
-                <h1>hoiirrf</h1>
-                <p>dfghjk</p>
-            </div>
+        <div className="flex justify-center">
+        <div className="flex  flex-col justify-center">
+           
+                <h1 className="text-4xl text-white border-b-4 mb-5 border-green-700 font-bold">About Me</h1>
+                <p className="text-2xl text-white">dfghjk</p>
+                </div>
+       
         </div>
     </section>
 }
--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -2,12 +2,12 @@ import Image from "../Image/girl.png"
 import { FaLinkedin } from "react-icons/fa";
 import { SiLeetcode } from "react-icons/si";
 export default function hero(){
-    return<section className='flex flex-col md:flex-row px-4 py-32 mobile-navd justify-center'>
+    return<section className='flex flex-col md:flex-row  mobile-navd justify-center'>
     <div className='md:w-1/2 flex flex-col'>
          <h1 className=' text-white text-6xl font-hero-font'>Hi, <br/> Im <span className='text-white'>Meena</span> Knadasamy
              {/* <p className='text-2xl'>{config.subtitle}</p> */}
          </h1>
-         <div className='flex py-4'>
+         <div className='flex py-4 md:justify-center'>
         <a href="" className="pr-4 text-white hover:text-gray-300"><FaLinkedin size={40}/></a>
         <a href="" className="pr-4 text-white hover:text-gray-300"><SiLeetcode size={40}/></a>
         </div>
